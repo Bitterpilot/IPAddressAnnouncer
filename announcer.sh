80f74f687f2cd6b06bf71415e06ddef314b6f1fc
Nathan Minissale <Nathan@saras-MacBook-Air.local>
2018-11-04 13:56:36 +0800

    work in progress

--- announcer.sh
+++ announcer.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+currentIP=`curl icanhazip.com`
+echo $currentIP
+
+wrap={"text":"${currentIP}"}
 
 curl -X POST -H 'Content-type: application/json' \
-    --data '{"text":"Hello, World!"}' \
+    --data '{"text":"${currentIP}"}' \
     https://hooks.slack.com/services/"${SLACKCRED}"
+
+
+# curl -X POST -H 'Content-type: application/json' \
+#     --data '{"text":"${currentIP}"}' \
+#     https://hooks.slack.com/services/"${SLACKCRED}"
