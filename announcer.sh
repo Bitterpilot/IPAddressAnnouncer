0d007d4facc68292374a7321a649fc4392d3f18e
Nathan Minissale <Nathan@saras-MacBook-Air.local>
2018-11-04 13:28:01 +0800

    broke up curl to make it easier to read

--- announcer.sh
+++ announcer.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' https://hooks.slack.com/services/"${SLACKCRED}"
+curl -X POST -H 'Content-type: application/json' \
+    --data '{"text":"Hello, World!"}' \
+    https://hooks.slack.com/services/"${SLACKCRED}"
